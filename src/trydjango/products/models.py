f5ddc98217ece27ce22add995352c7dd0317079f
HuoYinqi <983673234@qq.com>
2020-12-17 20:26:08 +0800

    create model named Product

--- src/trydjango/products/models.py
+++ src/trydjango/products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models.fields import DecimalField
 
 # Create your models here.
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=False, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=100)
+    summary = models.TextField()
+    featured = models.BooleanField()
