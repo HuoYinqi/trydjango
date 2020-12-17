156f441b4f9f99043ba3450ae5297a544892d84b
HuoYinqi <983673234@qq.com>
2020-12-17 09:22:22 +0800

    recreate project in different path

--- src/trydjango/trydjango/urls.py
+++ src/trydjango/trydjango/urls.py
@@ -1,4 +1,4 @@
-"""products URL Configuration
+"""trydjango URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
